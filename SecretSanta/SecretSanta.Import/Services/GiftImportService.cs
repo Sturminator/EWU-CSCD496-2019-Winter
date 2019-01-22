548bcd1d8c0a9afe7c65d4d79c2eee0b51d4d097
Chris <raeder929@hotmail.com>
2019-01-21 20:40:01 -0800

    Initial Import Files

---
+++ SecretSanta/SecretSanta.Import/Services/GiftImportService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Import.Services
+{
+    public class GiftImportService
+    {
+
+    }
+}
