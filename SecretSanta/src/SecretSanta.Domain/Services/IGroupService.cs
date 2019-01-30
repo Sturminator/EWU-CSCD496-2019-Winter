057f0a4b187888fa420250762ffcc87cc2179baa
Chris <raeder929@hotmail.com>
2019-01-29 23:34:41 -0800

    Restarting after issues with branch
    I should be able to catch up fairly fast, don't have much else going on Wednesday.

---
+++ SecretSanta/src/SecretSanta.Domain/Services/IGroupService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Domain.Services
+{
+    public interface IGroupService
+    {
+
+    }
+}
