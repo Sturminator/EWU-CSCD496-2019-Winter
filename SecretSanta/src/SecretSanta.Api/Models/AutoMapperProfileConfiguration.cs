d0eb966fdf3be78c234f3dd589e1e6b9029deca4
Chris <raeder929@hotmail.com>
2019-02-07 18:18:46 -0800

    almost forgot the automapper profile config

---
+++ SecretSanta/src/SecretSanta.Api/Models/AutoMapperProfileConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using SecretSanta.Api.ViewModels;
+using SecretSanta.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Api.Models
+{
+    public class AutoMapperProfileConfiguration : Profile
+    {
+        public AutoMapperProfileConfiguration()
+        {
+            CreateMap<Gift, GiftViewModel>();
+            CreateMap<GiftViewModel, Gift>();
+
+            CreateMap<User, UserViewModel>();
+            CreateMap<UserViewModel, User>();
+            CreateMap<UserInputViewModel, User>();
+
+            CreateMap<Group, GroupViewModel>();
+            CreateMap<GroupViewModel, Group>();
+            CreateMap<GroupInputViewModel, Group>();
+        }
+    }
+}
