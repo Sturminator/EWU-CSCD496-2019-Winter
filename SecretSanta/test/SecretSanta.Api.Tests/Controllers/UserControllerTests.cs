6ab29de4a2ea3687cb485497cc1045f426d77451
Chris <raeder929@hotmail.com>
2019-02-07 21:24:49 -0800

    added unit testing

--- SecretSanta/test/SecretSanta.Api.Tests/Controllers/UserControllerTests.cs
+++ SecretSanta/test/SecretSanta.Api.Tests/Controllers/UserControllerTests.cs
@@ -1,10 +1,50 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SecretSanta.Api.Controllers;
+using SecretSanta.Api.ViewModels;
+using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SecretSanta.Api.Tests.Controllers
 {
-    class UserControllerTests
+    [TestClass]
+    public class UserControllerTests
     {
+        [TestMethod]
+        public void CreateUser_ReturnsCreatedUser()
+        {
+            var user = new UserInputViewModel   
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var service = new Mock<IUserService>();
+            service.Setup(x => x.AddUser(It.Is<Domain.Models.User>(u => u.FirstName == u.FirstName)))
+                .Returns(new Domain.Models.User
+                {
+                    Id = 2,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName
+                })
+                .Verifiable();
+
+            var controller = new UserController(service.Object, Mapper.Instance);
+
+            var result = controller.Post(user);
+
+            var resultValue = (UserViewModel)((OkObjectResult)result).Value;
+
+            Assert.AreEqual(2, resultValue.Id);
+            Assert.AreEqual("John", resultValue.FirstName);
+            Assert.AreEqual("Doe", resultValue.LastName);
+            service.VerifyAll();
+        }
     }
 }
