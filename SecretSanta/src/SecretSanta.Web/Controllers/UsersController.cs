6dd0d5358f7390905b76e6e0cf54d82a76432223
Chris <raeder929@hotmail.com>
2019-02-27 15:38:39 -0800

    Finished up UserController

--- SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
@@ -10,10 +10,12 @@ namespace SecretSanta.Web.Controllers
     public class UsersController : Controller
     {
         private IHttpClientFactory ClientFactory { get; }
+
         public UsersController(IHttpClientFactory clientFactory)
         {
             ClientFactory = clientFactory;
         }
+
         public async Task<IActionResult> Index()
         {
             using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
@@ -29,6 +31,7 @@ namespace SecretSanta.Web.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Add(UserInputViewModel viewModel)
         {
@@ -50,6 +53,83 @@ namespace SecretSanta.Web.Controllers
                         ViewBag.ErrorMessage = se.Message;
                     }
                 }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Model State. Must have a First Name.";
+            }
+
+            return result;
+        }
+
+        [HttpGet]
+        public IActionResult Update(int userId)
+        {
+            ViewBag.UserId = userId;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UserInputViewModel viewModel, int userId)
+        {
+            IActionResult result = View();
+
+            if(ModelState.IsValid)
+            {
+                using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+                {
+                    try
+                    {
+                        var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                        await secretSantaClient.UpdateUserAsync(userId, viewModel);
+
+                        result = RedirectToAction(nameof(Index));
+                    }
+                    catch (SwaggerException se)
+                    {
+                        ViewBag.ErrorMessage = se.Message;
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Model State. Must have a First Name.";
+            }
+
+            return result;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Remove(int userId)
+        {
+            IActionResult result = View();
+
+            if(userId > 0)
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+                    {
+                        try
+                        {
+                            var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                            await secretSantaClient.DeleteUserAsync(userId);
+                            result = RedirectToAction(nameof(Index));
+                        }
+                        catch (SwaggerException se)
+                        {
+                            ViewBag.ErrorMessage = se.Message;
+                        }
+                    }
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Invalid Model State. Must have a First Name.";
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "User ID must be greater than 0.";
             }
 
             return result;
