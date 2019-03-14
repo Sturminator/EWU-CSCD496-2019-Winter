235f720c4c0af943e603d8ada3241d5c4d89e5aa
Chris <raeder929@hotmail.com>
2019-03-13 20:11:59 -0700

    Just some file setup to get a pull request up on github

---
+++ SecretSanta/SecretSanta.Web.UITests/GroupsPageTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecretSanta.Web.UITests
+{
+    [TestClass]
+    public class GroupsPageTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
